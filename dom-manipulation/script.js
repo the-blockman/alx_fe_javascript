7e7bac5bbf287ffaba98a810477558cec1d65893
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-12-13 09:25:19 +0100

    add createAddQuote function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -16,6 +16,30 @@ function showRandomQuote() {
   display.innerHTML = quote[randomIndex].text;
 }
 
+function createAddQuoteForm() {
+  const formDiv = document.createElement("div");
+
+  const quoteInput = document.createElement("input");
+  quoteInput.id = "newQuoteText";
+  quoteInput.type = "text";
+  quoteInput.placeholder = "Enter a new quote";
+
+  const quoteCategory = document.createElement("input");
+  quoteInput.id = "newQuoteCategory";
+  quoteInput.type = "text";
+  quoteInput.placeholder = "Enter quote category";
+
+  const addBtn = document.createElement("button");
+  addBtn.textContent = "Add Quote";
+  addBtn.addEventListener("click", addQuote);
+
+  formDiv.appendChild(quoteInput);
+  formDiv.appendChild(quoteCategory);
+  formDiv.appendChild(addBtn);
+
+  document.body.appendChild(formDiv);
+}
+
 function addQuote() {
   let newQuoteObject = {};
   newQuoteObject.text = newQuote.value;
